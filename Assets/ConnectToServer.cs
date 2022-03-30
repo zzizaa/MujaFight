42a04e2977ad330608f6940ab3af43c659a459db
zzizaa <gabriele.ziza@gmail.com>
2022-03-30 17:45:48 +0200

    added lobby scene and create and join match

--- Assets/ConnectToServer.cs
+++ Assets/ConnectToServer.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviour
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+        print("ConnectUsingSettings");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        print("OnConnectedToServer");
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        print("OnJoinedLobby");
+        SceneManager.LoadScene("Lobby");
     }
 }
